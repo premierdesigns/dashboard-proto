59a3d88cfc88ed05124a2853e4e4bba195c597ba
Chris Harris <clharris@premierdesigns.com>
2016-12-12 21:45:07 -0600

    Added more colors for the sidebar and more functionality comments on the rest of the pages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pd works!';
+  sideBarColor: string;
+  constructor() {
+    this.sideBarColor = 'purple';
+  }
 }
